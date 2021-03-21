a361a497a8b1e94dccef450e462ca55ddc91455e
TongeAdrien <67654635+TongeAdrien@users.noreply.github.com>
2021-03-21 16:33:55 -0700

    Cleaned up title
    Title no longer clips off canvas

--- uiTest.py
+++ uiTest.py
@@ -5,6 +5,6 @@ root = tkinter.Tk()
 
 #GUI styling
 canvas = tkinter.Canvas(root, height = 500, width = 500, bg = "#A569BD")
-canvas.create_text(10,10, text = "Welcome to Key Control" )
+canvas.create_text(70,10, text = "Welcome to Key Control" )
 #save styling
 canvas.pack()
