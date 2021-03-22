29439a51b6da82a31f0b16cc8ef14f9e10bcb893
TongeAdrien <67654635+TongeAdrien@users.noreply.github.com>
2021-03-21 19:48:07 -0700

    Added directional instructions to ui

--- uiTest.py
+++ uiTest.py
@@ -13,7 +13,11 @@ canvas.create_text(142,40, text = "Press 'P' to move cursor to top right corner 
 canvas.create_text(150,55, text = "Press 'Z' to move cursor to bottom left corner of screen")
 canvas.create_text(155,70, text = "Press 'M' to move cursor to bottom right corner of screen"  )
 canvas.create_text(128,85, text = "Press 'H' to move cursor to the centre of screen"  )
-
+#instructions to move cursor
+canvas.create_text(76,110, text = "Press 'W' to move cursor up" )
+canvas.create_text(76,125, text = "Press 'A' to move cursor left")
+canvas.create_text(82,140, text = "Press 'S' to move cursor down" )
+canvas.create_text(80,155, text = "Press 'D' to move cursor right")
 
 #save styling
 canvas.pack()
